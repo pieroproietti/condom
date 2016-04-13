3aea914d262990625243f2dce02ba098f19e4801
Piero Proietti <piero.proietti@gmail.com>
2016-04-13 10:55:52 +0200

    impostazione campi

--- parti_comuni/test.php
+++ parti_comuni/test.php
@@ -5,62 +5,62 @@ function affittiCreate($ds, $dd)
    echo "Creazione affitti; \r\n";
    $dd->query($dbstring);
    $dbstring = '
-   `Cod_appartamento` int(2) DEFAULT NULL,
-   `Cod_stabile` int(4) DEFAULT NULL,
-   `Proprietario_nome` varchar(200) DEFAULT NULL,
-   `Proprietario_intesta` varchar(200) DEFAULT NULL,
-   `Desriz_immobile` varchar(50) DEFAULT NULL,
-   `Indirizzo_immob` varchar(50) DEFAULT NULL,
-   `Cap` varchar(5) DEFAULT NULL,
-   `Citta` varchar(50) DEFAULT NULL,
-   `Pr` varchar(2) DEFAULT NULL,
-   `Nome_inquilino` varchar(50) DEFAULT NULL,
-   `Rendita_catastale` dbSingle4) DEFAULT NULL,
-   `Importo_fitto` decimal(8) DEFAULT NULL,
-   `Particella` varchar(50) DEFAULT NULL,
-   `Destinazione_d_uso` varchar(50) DEFAULT NULL,
-   `Inizio_contratto` datetime8) DEFAULT NULL,
-   `Ultimo_Rinnovo` datetime8) DEFAULT NULL,
-   `Prossima_scadenza` datetime8) DEFAULT NULL,
-   `Prossima_registrazione` datetime8) DEFAULT NULL,
-   `Note` dbMemo0) DEFAULT NULL,
-   `Def_ammin` varchar(50) DEFAULT NULL,
-   `Descr_1_voce` varchar(50) DEFAULT NULL,
-   `tipo_riga` varchar(50) DEFAULT NULL,
-   `Inte_cc` varchar(100) DEFAULT NULL,
-   `IBAN` varchar(27) DEFAULT NULL,
+      `cod_appartamento` int(2) DEFAULT NULL,
+      `cod_stabile` int(4) DEFAULT NULL,
+      `proprietario_nome` varchar(200) DEFAULT NULL,
+      `proprietario_intesta` varchar(200) DEFAULT NULL,
+      `desriz_immobile` varchar(50) DEFAULT NULL,
+      `indirizzo_immob` varchar(50) DEFAULT NULL,
+      `cap` varchar(5) DEFAULT NULL,
+      `citta` varchar(50) DEFAULT NULL,
+      `pr` varchar(2) DEFAULT NULL,
+      `nome_inquilino` varchar(50) DEFAULT NULL,
+      `rendita_catastale` decimal(10,2) DEFAULT NULL,
+      `importo_fitto` decimal(10,2) DEFAULT NULL,
+      `particella` varchar(50) DEFAULT NULL,
+      `destinazione_d_uso` varchar(50) DEFAULT NULL,
+      `inizio_contratto` datetime DEFAULT NULL,
+      `ultimo_rinnovo` datetime DEFAULT NULL,
+      `prossima_scadenza` datetime DEFAULT NULL,
+      `prossima_registrazione` datetime DEFAULT NULL,
+      `note` text DEFAULT NULL,
+      `def_ammin` varchar(50) DEFAULT NULL,
+      `descr_1_voce` varchar(50) DEFAULT NULL,
+      `tipo_riga` varchar(50) DEFAULT NULL,
+      `inte_cc` varchar(100) DEFAULT NULL,
+      `iban` varchar(27) DEFAULT NULL,
  ") ENGINE=InnoDB DEFAULT CHARSET=latin1; ';
 }
 
 function affittiCopy($ds, $dd)
 {
    $sql="SELECT ";
-   $sql.="Cod_appartamento, ";
-   $sql.="Cod_stabile, ";
-   $sql.="Proprietario_nome, ";
-   $sql.="Proprietario_intesta, ";
-   $sql.="Desriz_immobile, ";
-   $sql.="Indirizzo_immob, ";
-   $sql.="Cap, ";
-   $sql.="Citta, ";
-   $sql.="Pr, ";
-   $sql.="Nome_inquilino, ";
-   $sql.="Rendita_catastale, ";
-   $sql.="Importo_fitto, ";
-   $sql.="Particella, ";
-   $sql.="Destinazione_d_uso, ";
-   $sql.="Inizio_contratto, ";
-   $sql.="Ultimo_Rinnovo, ";
-   $sql.="Prossima_scadenza, ";
-   $sql.="Prossima_registrazione, ";
-   $sql.="Note, ";
-   $sql.="Def_ammin, ";
-   $sql.="Descr_1_voce, ";
-   $sql.="tipo_riga, ";
-   $sql.="Inte_cc, ";
-   $sql.="IBAN, ";
-   $sql .= 'FROM affitti ';
-   $sql .= 'WHERE 1';
+   $sql.="Cod_appartamento AS cod_appartamento, ";
+   $sql.="Cod_stabile AS cod_stabile, ";
+   $sql.="Proprietario_nome AS proprietario_nome, ";
+   $sql.="Proprietario_intesta AS proprietario_intesta, ";
+   $sql.="Desriz_immobile AS desriz_immobile, ";
+   $sql.="Indirizzo_immob AS indirizzo_immob, ";
+   $sql.="Cap AS cap, ";
+   $sql.="Citta AS citta, ";
+   $sql.="Pr AS pr, ";
+   $sql.="Nome_inquilino AS nome_inquilino, ";
+   $sql.="Rendita_catastale AS rendita_catastale, ";
+   $sql.="Importo_fitto AS importo_fitto, ";
+   $sql.="Particella AS particella, ";
+   $sql.="Destinazione_d_uso AS destinazione_d_uso, ";
+   $sql.="Inizio_contratto AS inizio_contratto, ";
+   $sql.="Ultimo_Rinnovo AS ultimo_rinnovo, ";
+   $sql.="Prossima_scadenza AS prossima_scadenza, ";
+   $sql.="Prossima_registrazione AS prossima_registrazione, ";
+   $sql.="Note AS note, ";
+   $sql.="Def_ammin AS def_ammin, ";
+   $sql.="Descr_1_voce AS descr_1_voce, ";
+   $sql.="tipo_riga AS tipo_riga, ";
+   $sql.="Inte_cc AS inte_cc, ";
+   $sql.="IBAN AS iban, ";
+   $sql.="FROM affitti ";
+   $sql.="WHERE 1";
    echo '<br/>';
    echo $sql;
    echo '<br/>';
--- vbscript/condom.vb
+++ vbscript/condom.vb
@@ -6,6 +6,7 @@ Public Function FieldNames() As String
     Dim sResult As String
 
     sTable = InputBox("Name of table?")
+    sTable = LCase(sTable)
     If sTable = "" Then
         Exit Function
     End If
@@ -15,15 +16,14 @@ Public Function FieldNames() As String
     sResult = "<?php " & vbCrLf
     sResult = sResult & "function " & sTable & "Create($ds, $dd)" & vbCrLf
     sResult = sResult & "{" & vbCrLf
-    sResult = sResult & "   $dbstring = 'drop table `affitti`;';" & vbCrLf
+    sResult = sResult & "   $dbstring = 'drop table `" & sTable & "`;';" & vbCrLf
     sResult = sResult & "   echo ""Creazione " & sTable & "; \r\n"";" & vbCrLf
     sResult = sResult & "   $dd->query($dbstring);" & vbCrLf
     sResult = sResult & "   $dbstring = '" & vbCrLf
     With rs
         For n = 0 To .Fields.Count - 1
-            sResult = sResult & "   `" & .Fields(n).Name & "` "
-            sResult = sResult & GetType(.Fields(n).Type)
-            sResult = sResult & .Fields(n).Size & ") DEFAULT NULL, " & vbCrLf
+            sResult = sResult & "      `" & LCase(.Fields(n).Name) & "` "
+            sResult = sResult & GetType(.Fields(n).Type, .Fields(n).size) & " DEFAULT NULL, " & vbCrLf
         Next 'n
         .Close
     End With
@@ -37,17 +37,17 @@ Public Function FieldNames() As String
     sResult = sResult & "{" & vbCrLf
     sResult = sResult & "   $sql=""SELECT "";" & vbCrLf
     For x = 0 To rs.Fields.Count - 1
-        sResult = sResult & "   $sql.=""" & rs.Fields(x).Name & ", "";" & vbCrLf
+        sResult = sResult & "   $sql.=""" & rs.Fields(x).Name & " AS " & LCase(rs.Fields(x).Name) & ", "";" & vbCrLf
     Next
-    sResult = sResult & "   $sql .= 'FROM " & sTable & " ';" & vbCrLf
-    sResult = sResult & "   $sql .= 'WHERE 1';" & vbCrLf
+    sResult = sResult & "   $sql.=""FROM " & sTable & " "";" & vbCrLf
+    sResult = sResult & "   $sql.=""WHERE 1"";" & vbCrLf
 
     sResult = sResult & "   echo '<br/>';" & vbCrLf
     sResult = sResult & "   echo $sql;" & vbCrLf
     sResult = sResult & "   echo '<br/>';" & vbCrLf
     sResult = sResult & "   $rows = $ds->query($sql, PDO::FETCH_ASSOC);" & vbCrLf
     sResult = sResult & "   foreach ($rows as $row) {" & vbCrLf
-    sResult = sResult & "      $dd->insert('affitti', $row);" & vbCrLf
+    sResult = sResult & "      $dd->insert('" & sTable & "', $row);" & vbCrLf
     sResult = sResult & "   }" & vbCrLf
     sResult = sResult & "}" & vbCrLf
 
@@ -61,78 +61,104 @@ Public Function FieldNames() As String
 
 End Function
 
-Function GetType(i)
+Function GetType(i, size)
+
+' Text    Use for text or combinations of text and numbers. 255 characters maximum
+' Memo    Memo is used for larger amounts of text. Stores up to 65,536 characters. Note: You cannot sort a memo field. However, they are searchable
+' Byte    Allows whole numbers from 0 to 255  1 byte
+' Integer Allows whole numbers between -32,768 and 32,767 2 bytes
+' Long    Allows whole numbers between -2,147,483,648 and 2,147,483,647   4 bytes
+' Single  Single precision floating-point. Will handle most decimals  4 bytes
+' Double  Double precision floating-point. Will handle most decimals  8 bytes
+' Currency    Use for currency. Holds up to 15 digits of whole dollars, plus 4 decimal places. Tip: You can choose which country's currency to use    8 bytes
+' AutoNumber  AutoNumber fields automatically give each record its own number, usually starting at 1  4 bytes
+' Date/Time   Use for dates and times 8 bytes
+' Yes/No  A logical field can be displayed as Yes/No, True/False, or On/Off. In code, use the constants True and False (equivalent to -1 and 0). Note: Null values are not allowed in Yes/No fields   1 bit
+' Ole Object  Can store pictures, audio, video, or other BLOBs (Binary Large OBjects) up to 1GB
+' Hyperlink   Contain links to other files, including web pages
+' Lookup Wizard   Let you type a list of options, which can then be chosen from a drop-down list  4 bytes
+
 
 Select Case i
+    '----------------------------------------
+    ' numeric
+    '----------------------------------------
+    ' integer
     Case dbBigInt
-    GetType = "dbBigInt"
-    GetType = "int("
+    GetType = "int(" & size & ")"
 
+    Case dbLong
+    GetType = "int(" & size & ")"
+
+    Case dbInteger
+    GetType = "int(" & size & ")"
+
+    ' decimal
+    Case dbCurrency
+    GetType = "decimal(10,2)"
+
+    Case dbDecimal
+    GetType = "decimal(10,2)"
+
+    Case dbDouble
+    GetType = "decimal(10,2)"
+
+    Case dbFloat
+    GetType = "decimal(10,2)"
+
+    Case dbNumeric
+    GetType = "decimal(10,2)"
+
+    Case dbSingle
+    GetType = "decimal(10,2)"
+
+
+    '----------------------------------------
+    ' binary
+    '----------------------------------------
     Case dbBinary
     GetType = "dbBinary"
 
     Case dbByte
     GetType = "dbByte"
 
-    Case dbChar
-    GetType = "dbChar"
-
-
-    Case dbCurrency
-    GetType = "dbCurrency"
-    GetType = "decimal("
-
-    Case dbDate
-    GetType = "datetime"
-
-    Case dbDecimal
-    GetType = "dbDecimal"
-    GetType = "decimal("
-
-    Case dbDouble
-    GetType = "dbDouble"
-    GetType = "decimal("
-
-    Case dbFloat
-    GetType = "dbFloat"
-
-    Case dbGUID
-    GetType = "dbGUID"
-
-    Case dbInteger
-    GetType = "dbInteger"
-    GetType = "int("
-
-    Case dbLong
-    GetType = "dbLong"
-    GetType = "int("
-
     Case dbLongBinary
     GetType = "dbLongBinary"
 
-    Case dbMemo
-    GetType = "dbMemo"
-
-    Case dbNumeric
-    GetType = "dbNumeric"
-
-    Case dbSingle
-    GetType = "dbSingle"
-
-    Case dbText
-    GetType = "dbText"
-    GetType = "varchar("
-
-    Case dbTime
-    GetType = "dbTime"
-    GetType = "datetime"
-
-
-    Case dbTimeStamp
-    GetType = "dbTimeStamp"
-    GetType = "datetime"
-
     Case dbVarBinary
     GetType = "dbVarBinary"
+
+    '----------------------------------------
+    ' char
+    '----------------------------------------
+    Case dbChar
+    GetType = "varchar(" & size & ")"
+
+    Case dbMemo
+    GetType = "text"
+
+    Case dbText
+    GetType = "varchar(" & size & ")"
+    'GetType = "tinytext"
+
+
+
+    '----------------------------------------
+    ' date
+    '----------------------------------------
+    Case dbDate
+    GetType = "datetime"
+
+    Case dbTime
+    GetType = "datetime"
+
+    Case dbTimeStamp
+    GetType = "datetime"
+
+
+    ' GUID
+    Case dbGUID
+    GetType = "dbGUID"
+
 End Select
 End Function
