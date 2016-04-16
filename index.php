d0b3740d66e55290ffd0376d153ea20ddcb10f0e
William Nonnis <willononnis@gmail.com>
2016-04-16 16:46:32 +0200

    boh

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
     <h1>Condom</h1>
     <h4>Un sistema "Sicuro" di gestione condomini!</h4>
     <?php
-    echo "<li><a href='192.168.1.203/phpmyadmin'>Source->phpmyadmin</a></li>";
-    echo "<li><a href='127.0.0.1/phpmyadmin'>Condom->phpmyadmin</a></li>";
+    echo "<li><a href='http://192.168.1.203/phpmyadmin'>Source->phpmyadmin</a></li>";
+    echo "<li><a href='http://127.0.0.1/phpmyadmin'>Condom->phpmyadmin</a></li>";
     echo '<br/>';
-    echo "<li><a href='parti_comuni'>parti_comuni</a></li>";
-    echo "<li><a href='generale_stabile'>generale_stabile</a></li>";
-    echo "<li><a href='singolo_anno'>singolo_anno</a></li>";
+    echo "<li><a href='parti_comuni'>Source/parti_comuni</a></li>";
+    echo "<li><a href='generale_stabile'>Source/generale_stabile</a></li>";
+    echo "<li><a href='singolo_anno'>Source/singolo_anno</a></li>";
     echo '<br/>';
-    echo "<li><a href='importazione_globale'>importazione_globale</a></li>";
+    echo "<li><a href='importazione_globale'>Condom/importazione_globale</a></li>";
 
      ?>
   </body>
