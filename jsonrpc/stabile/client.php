116784ecdcb3aebe162f1e1085a3e7d85dde862f
William Nonnis <willononnis@gmail.com>
2016-04-18 13:08:28 +0200

    Aggiunto $client->debug

--- jsonrpc/stabile/client.php
+++ jsonrpc/stabile/client.php
@@ -7,7 +7,7 @@ require 'fguillot/json-rpc/src/JsonRPC/ResponseException.php';
 use JsonRPC\Client;
 
 $stabile = new Client('http://192.168.1.203:8080/jsonrpc/stabile/server.php');
+$stabile->debug = true;
 
-// performs some basic operation
 echo 'Stabile: drop;<br/>'."\n";
 $stabile->execute('drop', [3, 5]);
