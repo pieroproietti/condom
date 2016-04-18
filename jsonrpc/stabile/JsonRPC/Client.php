2a2858dd59581f3c01be4b7678b022481a20898b
William Nonnis <willononnis@gmail.com>
2016-04-18 15:11:22 +0200

    aggiunto rb in apertura stream

--- jsonrpc/stabile/JsonRPC/Client.php
+++ jsonrpc/stabile/JsonRPC/Client.php
@@ -304,13 +304,12 @@ class Client
      */
     private function doRequest(array $payload)
     {
-        $stream = @fopen(trim($this->url), 'r', false, $this->getContext($payload));
+        $stream = @fopen(trim($this->url), 'rb', false, $this->getContext($payload));
 
-        /*
         if (! is_resource($stream)) {
             throw new ConnectionFailureException('Unable to establish a connection: ' . $this->url);
         }
-        */
+
         $metadata = stream_get_meta_data($stream);
         $this->handleHttpErrors($metadata['wrapper_data']);
 
