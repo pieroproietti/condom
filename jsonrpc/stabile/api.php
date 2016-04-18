e1dc4241239640a7173fb0d74030fe7ccb99fcd6
William Nonnis <willononnis@gmail.com>
2016-04-18 12:04:27 +0200

    poz

--- jsonrpc/stabile/api.php
+++ jsonrpc/stabile/api.php
@@ -12,13 +12,24 @@ class Api
 
     public function drop()
     {
-        require 'drop.php';
-        if (dropStabile($dbc)) {
-            return true;
+        $db = new mysqli($dbc['server'], $dbc['username'], $dbc['password']);
+
+        if ($db->connect_errno) {
+            echo 'Il sito sta avendo problemi...\n';
+            echo "Errore: connessione MySQL fallita: \n";
+            echo 'Errno: '.$db->connect_errno."\n";
+            echo 'Error: '.$db->connect_error."\n";
+
+            return false;
         } else {
-            throw new Exception('Errore: non riesco a cancellare il database: stabile');
+            echo 'connessi a: '.$db->host_info."\n";
+            $db->query('DROP DATABASE `'.$dbc['name'].';');
+            $db->close();
+
+            return true;
         }
     }
+
     public function create($dbc)
     {
         require 'create.php';
