377e578d208d701543eb394be719ffb6604e2165
William Nonnis <willononnis@gmail.com>
2016-04-18 10:01:32 +0200

    idem

--- jsonrpc/stabile/client.php
+++ jsonrpc/stabile/client.php
@@ -3,25 +3,13 @@ require_once '../includes/jsonRPCClient.php';
 $stabile = new jsonRPCClient('http://192.168.1.203:8080/jsonrpc/stabile/server.php');
 
 // performs some basic operation
-echo '<b>Esecuzione di drop</b><br/>'."\n";
+echo 'Stabile: drop</b><br/>'."\n";
 try {
 	if($stabile->drop){
 		echo "drop riuscito!<br/>";
 	}	else{
 		echo "drop NON riuscito!<br/>";
 	}
-
-;
 } catch (Exception $e) {
 	echo nl2br($e->getMessage()).'<br />'."\n";
 }
-
-// performs some strategic operation, locally allowed
-echo '<br /><b>Attempt to store strategic data</b><br />'."\n";
-try {
-	$myExample->writeSomething('Strategic string!');
-	echo 'Strategic data succefully stored';
-} catch (Exception $e) {
-	echo nl2br($e->getMessage());
-}
-?>
