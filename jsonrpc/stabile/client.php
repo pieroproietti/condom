e0386982c87e9a0b3a951634c24b791e66385fd6
William Nonnis <willononnis@gmail.com>
2016-04-18 11:01:20 +0200

    aggiust..

--- jsonrpc/stabile/client.php
+++ jsonrpc/stabile/client.php
@@ -6,12 +6,4 @@ $stabile = new Client('http://192.168.1.203:8080/jsonrpc/stabile/server.php');
 
 // performs some basic operation
 echo 'Stabile: drop;<br/>'."\n";
-try {
-	if($stabile->drop()){
-		echo "<li>drop riuscito!</li>";
-	}	else{
-		echo "<li>drop NON riuscito!</li>";
-	}
-} catch (Exception $e) {
-	echo nl2br($e->getMessage()).'<br />'."\n";
-}
+$stabile->execute('drop', [3, 5]);
